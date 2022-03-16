b21782e24474ce70060b7c34e60b0e7ae23ae0e7
Quinton Chester <quintonchester@gmail.com>
2022-03-16 11:53:06 -0500

    :hammer: Updated sampleSlice + test
    sampleSlice test for resetReducer was incorrect, also simplified object within initialState which required an update to the unit tests as well.

--- template/src/slices/sampleSlice.tsx
+++ template/src/slices/sampleSlice.tsx
@@ -2,39 +2,38 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface IState {
-	object: {
-		id: string;
-		value: string;
-		anotherValue: string;
-	};
-	_persist: any;
+    object: {
+        id: string;
+        value: string;
+    };
+    _persist: any;
 }
 
 export const initialState: IState = {
-	object: { id: '', value: '', anotherValue: '' },
-	_persist: '',
+    object: { id: '', value: '' },
+    _persist: '',
 };
 
 export const sampleSlice = createSlice({
-	name: 'sample',
-	initialState: initialState,
-	reducers: {
-		setObjectData: (state, action: PayloadAction<typeof state.object>) => {
-			state.object = action.payload;
-		},
-		updateObjectValue: (
-			state,
-			action: PayloadAction<Partial<typeof state.object>>,
-		) => {
-			state.object = { ...state.object, ...action.payload };
-		},
-		resetReducer: state => {
-			state = initialState;
-		},
-	},
+    name: 'sample',
+    initialState: initialState,
+    reducers: {
+        setObjectData: (state, action: PayloadAction<typeof state.object>) => {
+            state.object = action.payload;
+        },
+        updateObjectValue: (
+            state,
+            action: PayloadAction<Partial<typeof state.object>>,
+        ) => {
+            state.object = { ...state.object, ...action.payload };
+        },
+        resetReducer: () => {
+            return initialState;
+        },
+    },
 });
 
 export const { setObjectData, updateObjectValue, resetReducer } =
-	sampleSlice.actions;
+    sampleSlice.actions;
 
 export default sampleSlice.reducer;
